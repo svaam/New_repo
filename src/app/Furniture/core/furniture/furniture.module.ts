51ecb780c90223a8da37d9d9012f2e95429fa1ac
ANKITA GUPTA <ankitagupta@ANKITAs-MacBook-Air.local>
2019-08-28 00:08:54 +0530

    first commit

--- src/app/Furniture/core/furniture/furniture.module.ts
+++ src/app/Furniture/core/furniture/furniture.module.ts
@@ -8,6 +8,15 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import { SliderModule } from 'angular-image-slider';
+import { HeaderComponent } from '../../header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 
@@ -21,10 +30,18 @@ import { SliderModule } from 'angular-image-slider';
     MatGridListModule,
     MatCardModule,
     MatListModule,
-    SliderModule
+    SliderModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatTreeModule,
   ],
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
   ]
 })
 export class FurnitureModule { }
